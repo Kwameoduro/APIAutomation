5c3a21d79a704a496b7a62e5cdf8c8e6279595ae
Kwame Oduro <odurokwameee@gmail.com>
2025-07-11 02:04:02 +0000

    feat: add todo model with userid, title, and completed status

---
+++ src/test/java/com/restassuredapi/models/Todo.java
@@ -0,0 +1,55 @@
+package com.restassuredapi.models;
+
+
+
+
+public class Todo {
+    private int userId;
+    private int id;
+    private String title;
+    private boolean completed;
+
+    // Default constructor
+    public Todo() {}
+
+    // All-args constructor
+    public Todo(int userId, int id, String title, boolean completed) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    // Getters and setters
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
